3cb89c12125f0465a39cfc646ae6fdff19db869a
Murilo-Nep0muceno <murilonep@gmail.com>
2024-09-01 20:00:10 -0300

    more

---
+++ src/maratonajava/Bintroducaometodos/Calculadora.java
@@ -0,0 +1,8 @@
+package maratonajava.Bintroducaometodos;
+
+public class Calculadora {
+        public void somaDoisNumeros(){
+            System.out.println(10+10);
+        }
+
+}
---
+++ src/maratonajava/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,7 @@
+package maratonajava.Bintroducaometodos.test;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+
+    }
+}
