1b86e71b84bb26647f28aae752f509529e7e1a30
Murilo-Nep0muceno <murilonep@gmail.com>
2024-09-05 00:06:46 -0300

    More

--- src/maratonajava/Bintroducaometodos/dominio/Estudante.java
+++ src/maratonajava/Bintroducaometodos/dominio/Estudante.java
@@ -5,4 +5,10 @@ public class Estudante {
     public int idade;
     public String  sexo;
 
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+    }
 }
--- src/maratonajava/Bintroducaometodos/test/EstudanteTest01.java
+++ src/maratonajava/Bintroducaometodos/test/EstudanteTest01.java
@@ -5,23 +5,6 @@ import maratonajava.Bintroducaometodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
-        Estudante estudante01 = new Estudante();
-        Estudante estudante02 = new Estudante();
-        ImpressoraEstudante impressora = new ImpressoraEstudante();
-
-        estudante01.nome = "Saitama";
-        estudante01.idade = 26;
-        estudante01.sexo = "Macho";
-
-        estudante02.nome = "Hinata";
-        estudante02.idade = 26;
-        estudante02.sexo = "Woman";
-
-        impressora.imprime(estudante01);
-
-        impressora.imprime((estudante02));
-
-
 
 
     }
